31aec7192f00553261017a4f05b63bef9bb5d57f
Oindree Banerjee <banerjee.104@osu.edu>
2017-06-23 16:23:21 -0400

    oindree added doxygen comments to balloon.hh

--- balloon.hh
+++ balloon.hh
@@ -8,8 +8,6 @@
 
 #include <iostream>
 
-
-
 class Position;
 class Vector;
 class TChain;
@@ -38,138 +36,368 @@ private:
  
 public:
   Balloon();
+    
+    // GPS positions of Anita-lite balloon flight
+    int igps;                                                                  ///< which balloon position do we use out of the 25000 anitalite GPS positions.
+    int ibnposition;
+    double dtryingposition;                                                    ///< weighting factor: how many equivalent tries each neutrino counts for after having reduced possible interaction positions to within horizon
+    static const int MAX_POSITIONS=50;                                         ///< for the slac beam test
+    Vector slacpositions[MAX_POSITIONS];
+    string sslacpositions[MAX_POSITIONS];
+    int islacposition;
+    TChain *flightdatachain;
+    TTreeIndex *tindex;
+    unsigned int realTime_flightdata_temp;                                     ///< realtime from the flight data file
+    unsigned int realTime_flightdata;                                          ///< realtime from the flight data file
+    float flatitude,flongitude,faltitude,fheading,froll, fpitch;
+    double latitude,longitude,altitude,heading,roll,pitch;
+    double MINALTITUDE;                                                        ///< minimum altitude balloon needs to be before we consider it a good event to read from the flight data file
+    int igps_previous;                                                         ///< which entry from the flight data file the previous event was so we can just take the next one.
+    int REDUCEBALLOONPOSITIONS;                                                ///< only take every 100th entry in the flight data file
+    int WHICHPATH;                                                             ///< 0=fixed balloon position,1=randomized,2=ANITA-lite GPS data,3=banana plot
+    int RANDOMIZE_BN_ORIENTATION;                                              ///< 0=fixed balloon orientation,1=randomized
+    double BN_ALTITUDE;                                                        ///< pick balloon altitude
+    unsigned short surfTrigBandMask[9][2];                                     ///< Ryan's 16 bit masks for 9 surfs.  2x16 bit masks gives 32 channels per surf
+    float powerthresh[9][32];                                                  ///< power threshold in Watts
+    float meanp[9][32];                                                        ///< mean power in Watts
+    int CENTER;                                                                ///< whether or not to center one phi sector of the payload on the incoming signal (for making signal efficiency curves)
+    double altitude_bn;
+    double theta_bn;
+    double phi_bn;                                                             ///< theta,phi of balloon wrt south pole
+    Position r_bn;                                                             ///< position of balloon
+    double horizcoord_bn;                                                      ///< x component of balloon position
+    double vertcoord_bn;                                                       ///< y component of balloon position
+    Position r_boresights[Anita::NLAYERS_MAX][Anita::NPHI_MAX];                ///< position of antenna boresights
+    Vector x_axis_rot;
+    Vector y_axis_rot;
+    Vector z_axis_rot;
+    Vector n_bn;                                                               ///< normalized r_bn
+    Vector n_east;                                                             ///< east, as seen from the balloon position
+    Vector n_north;                                                            ///< north, as seen from the balloon position
+    double surface_under_balloon;                                              ///< distance between center of the earth and the surface of earth under balloon
+    Position r_bn_shadow;                                                      ///< position of the balloon projected on earth surface - point just below balloon at surface of the earth
+    double MAXHORIZON;                                                         ///< pick the interaction within this distance from the balloon so that it is within the horizon
+    double phi_spin;                                                           ///< orientation of the balloon
+    int NPOINTS;                                                               ///< number of GPS positions we're picking from.
+    int NPOINTS_MIN;                                                           ///< min and max index for gps positions we want to include in the simulation (to exclude launch and fall).  These are set in ReadFlight
+    int NPOINTS_MAX;
+    double latitude_bn_anitalite[100000];                                      ///< latitude at times along flightpath, equally distributed among gps data. This is filled with anita or anita-lite data, depending on which the user specifies
+    double longitude_bn_anitalite[100000];                                     ///< same for longitude
+    double altitude_bn_anitalite[100000];                                      ///< same for altitude
+    double heading_bn_anitalite[100000];                                       ///< same for heading of the balloon
+    double realtime_bn_anitalite[100000];                                      ///< same for real life time
+    double BN_LONGITUDE;                                                       ///< balloon longitude for fixed balloon location
+    double BN_LATITUDE;                                                        ///< balloon latitude for fixed balloon location
 
-  void setObservationLocation(Interaction *interaction1,int inu,IceModel *antarctic,Settings *settings1);
-  void GetBoresights(Settings *settings1,Anita *anita1,Position r_boresights[Anita::NLAYERS_MAX][Anita::NPHI_MAX]);
-  // GPS positions of Anita-lite balloon flight
-  int igps; // which balloon position do we use out of the 25000 anitalite GPS positions.
-  int ibnposition;
-  double dtryingposition; // weighting factor: how many equivalent tries each neutrino counts for after having reduced possible interaction positions to within horizon
-  
-  void PickDownwardInteractionPoint(Interaction *interaction1,Anita *anita1,Settings *settings1,IceModel *antarctica1,
+    /**
+     * This function sets the observation location
+     *
+     * This is a long description that I dont know yet
+     *
+     *
+     * @param  interaction1 -
+     * @param  inu -
+     * @param  antarctic -
+     * @param  settings1 -
+     * @return returns void
+     */
+     void setObservationLocation(Interaction *interaction1,int inu,IceModel *antarctic,Settings *settings1);
+    
+    /**
+     * This function gets the boresights
+     *
+     * This is a long description that I dont know yet
+     *
+     *
+     * @param  settings1 -
+     * @param  anita1 -
+     * @param  r_boresights[Anita::NLAYERS_MAX][Anita::NPHI_MAX] -
+     * @return returns void
+     */
+     void GetBoresights(Settings *settings1,Anita *anita1,Position r_boresights[Anita::NLAYERS_MAX][Anita::NPHI_MAX]);
+    
+    /**
+     * This function picks downward interaction point
+     *
+     * This is a long description that I dont know yet
+     *
+     *
+     * @param  interaction1 -
+     * @param  anita1 -
+     * @param  settings1 -
+     * @param  antarctica1 -
+     * @param  ray1 -
+     * @param  beyondhorizon -
+     * @return returns void
+     */
+     void PickDownwardInteractionPoint(Interaction *interaction1,Anita *anita1,Settings *settings1,IceModel *antarctica1,
 				    Ray *ray1, int &beyondhorizon); 
   
-  void InitializeBalloon();
-  void ReadAnitaliteFlight();
-  void CenterPayload(double& hitangle_e);
+    /**
+     * This function initializes the balloon or the specific flight
+     *
+     * This is a long description that I dont know yet
+     *
+     *
+     * @return returns void
+     */
+     void InitializeBalloon();
+    
+    /**
+     * This function reads in the ANITA LITE flight
+     *
+     * ANITA Lite is the first prototype ANITA flight
+     *
+     *
+     * @return returns void
+     */
+    void ReadAnitaliteFlight();
+    
+    /**
+     * This function centers the payload
+     *
+     * Long description
+     *
+     *
+     * @param  hitangle_e -
+     * @return returns void
+     */
+    void CenterPayload(double& hitangle_e);
 
-  void PickBalloonPosition(Vector straightup,IceModel *antarctica,Settings *settings1, Anita *anita1);
-  void PickBalloonPosition(IceModel *antarctica1,Settings *settings1,int inu,Anita *anita1, double randomNumber); // position of spot under balloon
+    
+    /**
+     * This function picks the balloon position
+     *
+     * Long description
+     *
+     *
+     * @param  straightup -
+     * @param  antarctica -
+     * @param  settings1 -
+     * @param  anita1 -
+     * @return returns void
+     */
+    void PickBalloonPosition(Vector straightup,IceModel *antarctica,Settings *settings1, Anita *anita1);
+    
+    /**
+     * This function picks the balloon position
+     *
+     * Position of spot under balloon
+     *
+     *
+     * @param  antarctica1 -
+     * @param  settings1 -
+     * @param  inu -
+     * @param  anita1 -
+     * @param  randomNumber -
+     * @return returns void
+     */
+    void PickBalloonPosition(IceModel *antarctica1,Settings *settings1,int inu,Anita *anita1, double randomNumber);
 
-  int Getibnposition();
+    /**
+     * This function gets ith balloon position
+     *
+     * Long description
+     *
+     *
+     * @return returns int
+     */
+    int Getibnposition();
 
-  double GetBalloonSpin(double heading); // get the azimuth of the balloon
+    /**
+     * This function gets the spin of the balloon whatever that means
+     *
+     * Get the azimuth of the balloon
+     *
+     *
+     * @param  heading -
+     * @return returns double
+     */
+    double GetBalloonSpin(double heading);
 
-  void GetAntennaOrientation(Settings *settings1, 
+    
+    /**
+     * This function gets the antenna orientation
+     *
+     * Long description
+     *
+     *
+     * @param  settings1 -
+     * @param  anita1 -
+     * @param  ilayer -
+     * @param  ifold
+     * @param  n_eplane -
+     * @param  n_hplane -
+     * @param  n_normal -
+     * @return returns void
+     */
+    void GetAntennaOrientation(Settings *settings1,
 			     Anita *anita1, 
 			     int ilayer, int ifold, 
-			     Vector& n_eplane, 
+			     Vector& n_eplane,
 			     Vector& n_hplane, 
 			     Vector& n_normal); 
 
-
-  void GetEcompHcompEvector(Settings *settings1, 
+    /**
+     * This function gets the e-component, h-component and e-vector
+     *
+     * Long description
+     *
+     *
+     * @param  settings1 -
+     * @param  n_eplane -
+     * @param  n_hplane -
+     * @param  n_pol
+     * @param  e_component -
+     * @param  h_component -
+     * @param  n_component -
+     * @return returns void
+     */
+    void GetEcompHcompEvector(Settings *settings1,
 			    Vector n_eplane, 
 			    Vector n_hplane, 
 			    const Vector n_pol, 
 			    double& e_component, 
 			    double& h_component, 
 			    double& n_component);
-
 		
-  void GetEcompHcompkvector(Vector n_eplane, 
+    /**
+     * This function gets the e-component, h-component and k-vector
+     *
+     * Long description
+     *
+     *
+     * @param  n_eplane -
+     * @param  n_hplane -
+     * @param  n_normal -
+     * @param  n_exit2bn -
+     * @param  e_component_kvector -
+     * @param  h_component_kvector - 
+     * @param  n_component_kvector -
+     * @return returns void
+     */
+    void GetEcompHcompkvector(Vector n_eplane,
 			    Vector n_hplane, 
 			    Vector n_normal, 
 			    const Vector n_exit2bn, 
 			    double& e_component_kvector, 
 			    double& h_component_kvector, 
 			    double& n_component_kvector);
-
-  void GetHitAngles(
+    
+    /**
+     * This function gets the hit angles
+     *
+     * Long description
+     *
+     *
+     * @param  e_component_kvector -
+     * @param  h_component_kvector -
+     * @param  n_component_kvector -
+     * @param  hitangle_e -
+     * @param  hitangle_h -
+     * @return returns void
+     */
+    void GetHitAngles(
 		    double e_component_kvector,
 		    double h_component_kvector,
 		    double n_component_kvector, 
 		    double& hitangle_e,
 		    double& hitangle_h); 
  
-  void SetDefaultBalloonPosition(IceModel *antarctica1);
+    /**
+     * This function sets the default balloon position
+     *
+     * Long description
+     *
+     *
+     * @param  antarctica1 -
+     * @return returns void
+     */
+    void SetDefaultBalloonPosition(IceModel *antarctica1);
 
-  void setr_bn(double latitude,double longitude);
+    /**
+     * This function sets r of the balloon
+     *
+     * Long description
+     *
+     *
+     * @param  latitude -
+     * @param  longitude -
+     * @return returns void
+     */
+    void setr_bn(double latitude,double longitude);
 
-  void AdjustSlacBalloonPosition(int inu); // move payload around like we did at slac
-  void GetSlacPositions(Anita *anita1); 
-
-
-
-
-  // for the slac beam test
-  static const int MAX_POSITIONS=50;
-  Vector slacpositions[MAX_POSITIONS];
-  string sslacpositions[MAX_POSITIONS];
-  int islacposition;
-
-  TChain *flightdatachain;
-
-  TTreeIndex *tindex;
-
-  unsigned int realTime_flightdata_temp; // realtime from the flight data file
-  unsigned int realTime_flightdata; // realtime from the flight data file
-   
-  float flatitude,flongitude,faltitude,fheading,froll, fpitch;
-  double latitude,longitude,altitude,heading,roll,pitch;
-  double MINALTITUDE; // minimum altitude balloon needs to be before we consider it a good event to read from the flight data file
-  int igps_previous;  // which entry from the flight data file the previous event was so we can just take the next one.
-  int REDUCEBALLOONPOSITIONS; // only take every 100th entry in the flight data file
-  int WHICHPATH; // 0=fixed balloon position,1=randomized,2=ANITA-lite GPS data,3=banana plot
-  int RANDOMIZE_BN_ORIENTATION;// 0=fixed balloon orientation,1=randomized
-  double BN_ALTITUDE; // pick balloon altitude
-  unsigned short surfTrigBandMask[9][2]; // Ryan's 16 bit masks for 9 surfs.  2x16 bit masks gives 32 channels per surf
-  float powerthresh[9][32]; // power threshold in Watts
-  float meanp[9][32]; // mean power in Watts
-
-  int CENTER; // whether or not to center one phi sector of the payload on the incoming signal (for making signal efficiency curves)
-
-  double altitude_bn;
-  double theta_bn;
-  double phi_bn;// theta,phi of balloon wrt south pole
-  Position r_bn; // position of balloon
-  double horizcoord_bn; // x component of balloon position
-  double vertcoord_bn; // y component of balloon position
-  Position r_boresights[Anita::NLAYERS_MAX][Anita::NPHI_MAX]; //position of antenna boresights
-
-  void GetBoresights(Settings *settings1,Anita *anita1);
-  Vector RotatePayload(Vector ant_pos);//rotate from payload coord to earth coord
-  Vector unRotatePayload(Vector ant_pos);//rotate from earth to payload coord. (undoes RotatePayload)
-  void calculate_antenna_positions(Settings *settings1,Anita *anita1);// this calculates the above 
-
-  Vector x_axis_rot;
-  Vector y_axis_rot;
-  Vector z_axis_rot;
-  Vector n_bn; // normalized r_bn
-  Vector n_east; // east, as seen from the balloon position
-  Vector n_north; // north, as seen from the balloon position
-  double surface_under_balloon; // distance between center of the earth and the surface of earth under balloon
-  Position r_bn_shadow;//position of the balloon projected on earth surface - point just below balloon at surface of the earth
-  double MAXHORIZON;                // pick the interaction within this distance from the balloon so that it is within the horizon
-  double phi_spin; // orientation of the balloon
-
-  int NPOINTS; // number of GPS positions we're picking from.
-  int NPOINTS_MIN; // min and max index for gps positions we want to include in the simulation (to exclude launch and fall).  These are set in ReadFlight
-  int NPOINTS_MAX;
-
-  double latitude_bn_anitalite[100000]; // latitude at times along flightpath, equally distributed among gps data. This is filled with anita or anita-lite data, depending on which the user specifies
-  double longitude_bn_anitalite[100000]; // same for longitude
-  double altitude_bn_anitalite[100000]; // same for altitude
-  double heading_bn_anitalite[100000]; // same for heading of the balloon
-  double realtime_bn_anitalite[100000]; // same for real life time
-
+    /**
+     * This function adjusts the slac balloon position
+     *
+     * move payload around like we did at slac
+     *
+     *
+     * @param  inu -
+     * @return returns void
+     */
+    void AdjustSlacBalloonPosition(int inu);
     
-  double BN_LONGITUDE; //balloon longitude for fixed balloon location
-  double BN_LATITUDE; //balloon latitude for fixed balloon location
+    /**
+     * This function gets the slac balloon positions
+     *
+     * Long description
+     *
+     *
+     * @param  anita1 -
+     * @return returns void
+     */
+    void GetSlacPositions(Anita *anita1);
+
+    /**
+     * This function gets boresights
+     *
+     * Long description
+     *
+     *
+     * @param  settings1 -
+     * @param  anita1 -
+     * @return returns void
+     */
+    void GetBoresights(Settings *settings1,Anita *anita1);
+    
+    /**
+     * This function calculates antenna positions
+     *
+     * Long description
+     *
+     *
+     * @param  settings1 -
+     * @param  anita1 -
+     * @return returns void
+     */
+    void calculate_antenna_positions(Settings *settings1,Anita *anita1);
+    
+    /**
+     * This function rotates the payload
+     *
+     * Rotate from payload coord to earth coord
+     *
+     *
+     * @param  ant_pos -
+     * @return returns vector
+     */
+    Vector RotatePayload(Vector ant_pos);
+    
+    /**
+     * This function UN-rotates the payload
+     *
+     * Rotate from earth to payload coord. (undoes RotatePayload)
+     *
+     *
+     * @param  ant_pos -
+     * @return returns vector
+     */
+    Vector unRotatePayload(Vector ant_pos);
+
+
+   
 }; //class Balloon
-
-
-
 
 const string anitaliteflight="data/BalloonGPS.txt"; // the gps path of the anita-lite flight
 const string anitaflight="data/anitagps.txt";// gps path of anita flight
