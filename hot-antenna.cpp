a75ef8c9a872bf52c94106d9a08ddb5dd0b480b2
Viatcheslav Bugaev <viatcheslav.bugayov@gmail.com>
2018-09-27 18:21:03 -0500

    Plotting antenna gain.

--- hot-antenna.cpp
+++ hot-antenna.cpp
@@ -24,6 +24,7 @@
 #include "Settings.h"
 #include "anita.hh"
 #include "balloon.hh"
+#include "Constants.h"
 #include "hot-antenna.h"
 
 extern struct xlibstruct *gxlib;
@@ -35,6 +36,7 @@ enum RunMode { m_init, m_reload, m_step, NRunModes };
 extern Settings* global_settings1;
 extern Anita *global_anita1;
 extern Balloon *global_bn1;
+extern Vector direction2bn;
 
 // bInteractive: can be assigned "false" once through game_init
 // to instruct the module that it is in the batch mode.
@@ -87,24 +89,42 @@ void PlotGain(std::map<std::string, void*> *penv, RunMode mode, struct nk_contex
 
   if (mode == m_init) return;
 
+  const double Dir2BalLen = 5;
+  const int SelAntLayer = 3;
+  const int SelAntFold = 0;
+  Vector n_eplane, SelAnt_eplane;
+  Vector n_hplane, SelAnt_hplane;
+  Vector n_normal, SelAnt_normal;
+
   static bvv::TBuffer <int> SelLayer(0);
   
   INIT_VAR_ONCE(TCanvas, cHotTest, new TCanvas());
+  INIT_VAR_ONCE(TCanvas, cGain, new TCanvas());
   RESET_VAR_ONRELOAD(TView, view, TView::CreateView(1));
   RESET_VAR_ONRELOAD(vector <TPolyLine3D>, vAntNormals, new vector <TPolyLine3D> (48));
   RESET_VAR_ONRELOAD(vector <TPolyMarker3D>, vAntPos, new vector <TPolyMarker3D> (48));
+  RESET_VAR_ONRELOAD(TPolyLine3D, lDir2Bal, new TPolyLine3D(2));
+  RESET_VAR_ONRELOAD(TPolyLine3D, lSelAnt_hplane, new TPolyLine3D(2));
+  RESET_VAR_ONRELOAD(TPolyLine3D, lSelAnt_eplane, new TPolyLine3D(2));
+  RESET_VAR_ONRELOAD(TGraph, gegain, new TGraph(Anita::NFREQ));
+  RESET_VAR_ONRELOAD(TGraph, ghgain, new TGraph(Anita::NFREQ));
 
+  cHotTest->cd();
   if (mode == m_reload) {
+    int ReturnCode;
+
+    global_bn1->GetAntennaOrientation(global_settings1,  global_anita1,  SelAntLayer,  SelAntFold, SelAnt_eplane,  SelAnt_hplane,  SelAnt_normal);
+    
     const double RangeMax = 5;
     view->SetRange(-RangeMax, -RangeMax, -RangeMax, +RangeMax, +RangeMax, +RangeMax);
-
+    lDir2Bal->SetPoint(0, 0, 0, 0);
+    lDir2Bal->SetPoint(1, SelAnt_normal[0] * Dir2BalLen, SelAnt_normal[1] * Dir2BalLen, SelAnt_normal[2] * Dir2BalLen);
+    lDir2Bal->SetLineColor(kMagenta);
+    lDir2Bal->Draw();
     for (int ilayer=0; ilayer < global_settings1->NLAYERS; ilayer++) { // loop over layers on the payload
       for (int ifold=0;ifold<global_anita1->NRX_PHI[ilayer];ifold++) { // ifold loops over phi
         int antNum = global_anita1->GetRxTriggerNumbering(ilayer, ifold);
-        cout << "ilayer: " << ilayer << ", ifold: " << ifold << ", antNum: " << antNum << endl;
-        Vector n_eplane;
-        Vector n_hplane;
-        Vector n_normal;
+        // cout << "ilayer: " << ilayer << ", ifold: " << ifold << ", antNum: " << antNum << endl;
 
         global_bn1->GetAntennaOrientation(global_settings1,  global_anita1,  ilayer,  ifold, n_eplane,  n_hplane,  n_normal);
         // vAntNormals->at(antNum) = TPolyLine3D(2);
@@ -114,6 +134,18 @@ void PlotGain(std::map<std::string, void*> *penv, RunMode mode, struct nk_contex
         vAntPos->at(antNum).SetPoint(0, antX, antY, antZ);
         vAntNormals->at(antNum).SetPoint(0, antX, antY, antZ);
         vAntNormals->at(antNum).SetPoint(1, antX + n_normal[0], antY + n_normal[1], antZ + n_normal[2]);
+        if (ilayer == SelAntLayer && ifold == SelAntFold) {
+          lSelAnt_eplane->SetPoint(0, antX, antY, antZ);
+          lSelAnt_eplane->SetPoint(1, antX + SelAnt_eplane[0], antY + SelAnt_eplane[1], antZ + SelAnt_eplane[2]);
+
+          lSelAnt_hplane->SetPoint(0, antX, antY, antZ);
+          lSelAnt_hplane->SetPoint(1, antX + SelAnt_hplane[0], antY + SelAnt_hplane[1], antZ + SelAnt_hplane[2]);
+
+          lSelAnt_eplane->Draw();
+          lSelAnt_hplane->Draw();
+          cout << "antXYZ: " << antX << ", " << antY << ", " << antZ << endl;
+          cout << "SelAnt_hplane[123]: " << SelAnt_hplane[0] << ", " << SelAnt_hplane[1] << ", " << SelAnt_hplane[2] << endl;
+        }
 
         vAntPos->at(antNum).SetMarkerSize(1);
         vAntPos->at(antNum).SetMarkerColor(kBlue);
@@ -123,13 +155,35 @@ void PlotGain(std::map<std::string, void*> *penv, RunMode mode, struct nk_contex
 
         vAntNormals->at(antNum).Draw();
         vAntPos->at(antNum).Draw();
-        std::cout << "n_normal: " << n_normal << std::endl;
+        // std::cout << "n_normal: " << n_normal << std::endl;
       }
     }
-    
+
+    // Computing gains:
+    cGain->cd(); 
+    double e_component_kvector, h_component_kvector, n_component_kvector;
+    global_bn1->GetEcompHcompkvector(SelAnt_eplane,  SelAnt_hplane,  SelAnt_normal, /* -SelAnt_hplane */ -SelAnt_normal /* <-- direction2bn */, e_component_kvector,  h_component_kvector,  n_component_kvector);
+    double e_component, h_component, n_component;
+    global_bn1->GetEcompHcompEvector(global_settings1,  SelAnt_eplane,  SelAnt_hplane, /* SelAnt_eplane */ SelAnt_hplane /* <-- polarization */,  e_component,  h_component,  n_component);
+    double hitangle_e, hitangle_h;
+    global_bn1->GetHitAngles(e_component_kvector, h_component_kvector, n_component_kvector, hitangle_e, hitangle_h);
+    // double e_signal[global_anita1->NFREQ] = {1.0}, h_signal[global_anita1->NFREQ] = {1.0};
+
+    for (int ifreq = 0; ifreq < Anita::NFREQ; ifreq++){
+      double e_signal = 1.0, h_signal = 1.0;
+      global_anita1->AntennaGain(global_settings1, hitangle_e, hitangle_h, e_component, h_component, ifreq, e_signal, h_signal);
+      gegain->SetPoint(ifreq, ifreq, e_signal);
+      ghgain->SetPoint(ifreq, ifreq, h_signal);
+      // cout << "hitangles: " << hitangle_e << ", " << hitangle_h << endl;
+      cout << "eh_signal: " << e_signal << ", " << h_signal << endl;
+    }
+    gegain->SetLineColor(kBlue);
+    ghgain->SetLineColor(kRed);
+    ghgain->Draw("AL");
+    gegain->Draw("L");
   }
 
-
+  cHotTest->cd();
   if (mode == m_step) {
     nk_layout_row_dynamic(ctx, 25, 1);
     property_int(ctx, "SelLayer: ", 0, SelLayer, global_settings1->NLAYERS - 1/*max*/, 1 /*increment*/, 0.5 /*sensitivity*/);
@@ -145,19 +199,25 @@ void PlotGain(std::map<std::string, void*> *penv, RunMode mode, struct nk_contex
             NormalsColor = kBlue;
           vAntNormals->at(antNum).SetLineColor(NormalsColor);
           vAntPos->at(antNum).SetMarkerColor(NormalsColor);
-          // vAntNormals->at(antNum).Draw();
+
+          if (ilayer == SelAntLayer && ifold == SelAntFold){
+            vAntNormals->at(antNum).SetLineColor(kMagenta);
+            vAntPos->at(antNum).SetMarkerColor(kMagenta);
+          }
+          vAntNormals->at(antNum).Draw();
         }
       }
     }
+    int ReturnCode;
+    // view->SetView(view->GetLongitude() + 0.4, view->GetLatitude() + 0, 0, ReturnCode);
+    cHotTest->Modified();
+    cHotTest->Update();
   }
-  // bn1->GetEcompHcompkvector(n_eplane,  n_hplane,  n_normal,  panel1->GetVec2bln(jpt), e_component_kvector,  h_component_kvector,  n_component_kvector);
-  // bn1->GetEcompHcompEvector(settings1,  n_eplane,  n_hplane,  panel1->GetPol(jpt),  e_component,  h_component,  n_component);
-  // bn1->GetHitAngles(e_component_kvector, h_component_kvector, n_component_kvector, hitangle_e, hitangle_h);
-
-  // anita1->AntennaGain(settings1, hitangle_e, hitangle_h, e_component, h_component, k, tmp_vhz[0][k], tmp_vhz[1][k]);
   if (mode == m_reload || *SelLayer) {
     cHotTest->Modified();
     cHotTest->Update();
+    cGain->Modified();
+    cGain->Update();
   }
 }
 
