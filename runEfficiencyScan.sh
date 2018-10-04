df51fbe2e4bb2827ab7b1e384ee922c6d33696f3
Linda Cremonesi <l.cremonesi@ucl.ac.uk>
2018-10-04 13:53:36 +0100

    Update input files

--- runEfficiencyScan.sh
+++ runEfficiencyScan.sh
@@ -11,9 +11,9 @@ for att in `seq -55 -40`;do
     echo "Off-axis attenuation: 0, 0, $att, $att, 0 # Attenuation applied to central phi sectors and the two adjecent ones (when 999 no signal in those antennas)" >> ${inputFile}
 
     att=$(($att * -1))
-    output=efficiencyScanPulseAtAntennaNewTriggerBenRotter-7dBNewNoise/Att${att}db
+    output=efficiencyScanPulseAtAntenna_ANITA3/Att${att}db
     mkdir -p ${output}
     echo $output
-    ./testInputAfterAntenna -i ${inputFile} -o ${output} -r 1 -n 100    
+    ./testInputAfterAntenna -i ${inputFile} -o ${output} -r 1 -n 200    
 
 done
