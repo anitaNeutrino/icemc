2c3709d5495473971fe2584764fdd004db848ec7
Viatcheslav Bugaev <viatcheslav.bugayov@gmail.com>
2018-12-09 14:56:34 -0600

    Creating eps plot comparing voltages.
    Comparing them form ApplyAntennaGain and TriggerPath.

--- SCRIPTS/gdb-tmp_volts-plot.sh
+++ SCRIPTS/gdb-tmp_volts-plot.sh
@@ -1,13 +1,16 @@
 #!/usr/bin/env bash
 
 GAIN_VOLTS_COL_FILE=$1
-GAIN_VS_TRIGGER_VOLTS=$2
+TRIG_VOLTS_COL_FILE=$2
+GAIN_VS_TRIGGER_VOLTS=$3
 
 TMP_GNUPLOT_SCRIPT=GDB/amp_eq_1/_gdb-tmp_volts-plot.gpl
 cat << EOF  > $TMP_GNUPLOT_SCRIPT
 set out "$GAIN_VS_TRIGGER_VOLTS"
 set term post color solid 18
-plot "$GAIN_VOLTS_COL_FILE" with lines
+plot [200: 300]\
+       	"$GAIN_VOLTS_COL_FILE" with lines,\
+       	"$TRIG_VOLTS_COL_FILE" with lines
 EOF
 
 gnuplot --persist $TMP_GNUPLOT_SCRIPT
--- SCRIPTS/wrap-gdb-dump.sh
+++ SCRIPTS/wrap-gdb-dump.sh
@@ -3,6 +3,56 @@
 MD5_OUT_PREF=parent ~/PROGS/DepTrack/with-md5.sh --snapshot-only --inp $0
 read SELF < val-parent-ID0-SNAPSHOT.out
 
+
+EXE=SCRIPTS/gdb-dump-v_banding_rfcm_forfft.sh
+INP=./.gdbinit
+OUT=GDB/amp_eq_1/v_banding_rfcm_forfft.dump
+
+MD5_OUT_PREF=rfcm ~/PROGS/DepTrack/with-md5.sh \
+ --envar PARENT_SCRIPT=$SELF \
+ --envar PARENT_LINENO=$LINENO \
+ --devar PATH=/usr/bin \
+ --devar HOME=$HOME \
+ --devar ICEMC_SRC_DIR=$ICEMC_SRC_DIR \
+ --devar LD_LIBRARY_PATH=$ANITA_UTIL_INSTALL_DIR/lib \
+ --inp $INP \
+ --inp $EXE \
+ --out $OUT \
+ $EXE 
+
+read MD5 < val-rfcm-ID0-MD5.out
+
+EXE=SCRIPTS/column-from-parlist.sh
+INP=GDB/amp_eq_1/md5-${MD5}_v_banding_rfcm_forfft.dump
+OUT=GDB/amp_eq_1/v_banding_rfcm_forfft_col.dat
+
+MD5_OUT_PREF=rfcm ~/PROGS/DepTrack/with-md5.sh \
+ --envar PARENT_SCRIPT=$SELF \
+ --envar PARENT_LINENO=$LINENO \
+ --devar PATH=/usr/bin \
+ --inp $EXE \
+ --inp $INP \
+ --out $OUT \
+ $EXE $INP $OUT
+
+read MD5_RFCM_COL < val-rfcm-ID0-MD5.out
+# exit 0
+
+
+# read MD5 < val-rfcm-ID0-MD5.out
+# INP=GDB/amp_eq_1/md5-${MD5}_v_banding_rfcm_forfft_col.dat
+# OUT=GDB/amp_eq_1/tmp_volts-cmp.eps
+# EXE=SCRIPTS/gdb-tmp_volts-plot.sh
+# MD5_OUT_PREF=rfcm ~/PROGS/DepTrack/with-md5.sh \
+#  --envar PARENT_SCRIPT=$SELF \
+#  --envar PARENT_LINENO=$LINENO \
+#  --devar PATH=/usr/bin \
+#  --inp $EXE \
+#  --inp $INP \
+#  --out $OUT \
+#  $EXE $INP $OUT
+
+
 EXE=SCRIPTS/gdb-dump.sh
 
 MD5_OUT_PREF=dump ~/PROGS/DepTrack/with-md5.sh \
@@ -31,8 +81,10 @@ MD5_OUT_PREF=dump ~/PROGS/DepTrack/with-md5.sh \
  --out $OUT \
  $EXE $INP $OUT
 
+
 read MD5 < val-dump-ID0-MD5.out
-INP=GDB/amp_eq_1/md5-${MD5}_tmp_volts_col.dat
+INP1=GDB/amp_eq_1/md5-${MD5}_tmp_volts_col.dat
+INP2=GDB/amp_eq_1/md5-${MD5_RFCM_COL}_v_banding_rfcm_forfft_col.dat
 OUT=GDB/amp_eq_1/tmp_volts-cmp.eps
 EXE=SCRIPTS/gdb-tmp_volts-plot.sh
 MD5_OUT_PREF=dump ~/PROGS/DepTrack/with-md5.sh \
@@ -40,9 +92,10 @@ MD5_OUT_PREF=dump ~/PROGS/DepTrack/with-md5.sh \
  --envar PARENT_LINENO=$LINENO \
  --devar PATH=/usr/bin \
  --inp $EXE \
- --inp $INP \
+ --inp $INP1 \
+ --inp $INP2 \
  --out $OUT \
- $EXE $INP $OUT
+ $EXE $INP1 $INP2 $OUT
 
 
 read MD5 < val-dump-ID0-MD5.out
