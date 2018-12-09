79dd30fa5af99daa809644ee176b1e561619793a
Viatcheslav Bugaev <viatcheslav.bugayov@gmail.com>
2018-12-08 23:18:04 -0600

    Scripts for with-md5 framework.

---
+++ SCRIPTS/gdb-dump.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+gdb ./testEAS-f603 -q -batch \
+  -ex "set print repeats 0" \
+  -ex "set height 0" \
+  -ex "set print elements 50000" \
+  -ex "set logging file GDB/amp_eq_1/tmp_volts.dump" \
+  -ex "set logging on" \
+  -ex "set logging redirect on" \
+  -ex "b ChanTrigger.cc:799" \
+  -ex "run" \
+  -ex "echo BIZDOOMCAR\n" \
+  -ex "output tmp_volts[0]"
---
+++ SCRIPTS/wrap-gdb-dump.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+MD5_OUT_PREF=parent ~/PROGS/DepTrack/with-md5.sh --snapshot-only --inp $0
+read SELF < val-parent-ID0-SNAPSHOT.out
+
+EXE=SCRIPTS/gdb-dump.sh
+
+MD5_OUT_PREF=dump ~/PROGS/DepTrack/with-md5.sh \
+ --devar PATH=/usr/bin \
+ --devar HOME=$HOME \
+ --devar ICEMC_SRC_DIR=$ICEMC_SRC_DIR \
+ --devar LD_LIBRARY_PATH=$ANITA_UTIL_INSTALL_DIR/lib \
+ --inp $EXE \
+ --out GDB/amp_eq_1/tmp_volts.dump \
+ $EXE 
+
+read MD5 < val-dump-ID0-MD5.out
+# echo "MD5 is: $MD5"
+# ls -lh GDB/amp_eq_1/md5-${MD5}_tmp_volts.dump
+
+EXE=SCRIPTS/column-from-parlist.sh
+INP=GDB/amp_eq_1/md5-${MD5}_tmp_volts.dump
+OUT=GDB/amp_eq_1/tmp_volts_col.dat
+
+MD5_OUT_PREF=dump ~/PROGS/DepTrack/with-md5.sh \
+ --envar PARENT_SCRIPT=$SELF \
+ --envar PARENT_LINENO=$LINENO \
+ --devar PATH=/usr/bin \
+ --inp $EXE \
+ --inp $INP \
+ --out $OUT \
+ $EXE $INP $OUT
