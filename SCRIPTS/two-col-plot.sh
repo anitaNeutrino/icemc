805d112586fc0b0e9909e541e6ac1ea1f92d5097
Viatcheslav Bugaev <viatcheslav.bugayov@gmail.com>
2019-02-03 00:07:18 -0600

    Untested, but seemingly correct plot generation.

--- SCRIPTS/two-col-plot.sh
+++ SCRIPTS/two-col-plot.sh
@@ -1,13 +1,11 @@
 #!/usr/bin/env bash
 
-FILE1=$1
-FILE2=$2
-OUT=$3
-XRANGE=$4
-YRANGE=$5
-SCALE=$6
-SHIFT=$7
-TMP_GNUPLOT_SCRIPT=$8
+NINP=0
+INP=none
+OUT=none
+XRANGE=none
+YRANGE=none
+TMP_GNUPLOT_SCRIPT=none
 
 function display_help()
 {
@@ -22,18 +20,84 @@ then
     exit 100 # Mandatory input parameters are missing.
 fi
 
+function reset()
+{
+    XSCALE=1.0
+    YSCALE=1.0
+    XSHIFT=+0.0
+    YSHIFT=+0.0
+    LINETYPE=none
+    LINEWIDTH=none
+}
+
+PLOTACC="plot"
+LINEACC=""
+
+function flush_line()
+{
+    local LINE_SEP="$1"
+    local LT=""
+    test "$LINETYPE" != "none" && LT=" lt $LINETYPE"
+    local LW=""
+    test "$LINEWIDTH" != "none" && LW=" lw $LINEWIDTH"
+    local APPENDIX="\"$INP\" (column(0) * $XSCALE $XSHIFT):(column(1) * $YSCALE $YSHIFT) with line$LT$LW""$LINE_SEP"
+    LINEACC="${LINEACC}$APPENDIX" 
+}
+
+reset
+
 while [[ $# > 0 ]]
 do
     case "$1" in
+      --xrange)
+         XRANGE="$2"
+         shift 2
+         ;;
+      --yrange)
+         YRANGE="$2"
+         shift 2
+         ;;
+      --lt)
+         LINETYPE="$2"
+         shift 2
+         ;;
+      --lw)
+         LINEWIDTH="$2"
+         shift 2
+         ;;
+      --xshift)
+         XSHIFT="$2"
+         shift 2
+         ;;
+      --yshift)
+         YSHIFT="$2"
+         shift 2
+         ;;
+      --xscale)
+         XSCALE="$2"
+         shift 2
+         ;;
+      --yscale)
+         YSCALE="$2"
+         shift 2
+         ;;
       --linkify)
         LINKIFY=yes
         shift 1
         ;;
       --inp) # Input file.
-         # Either a dependency file or a file with a list of files to be pointed by symbolic links in the directory specified by the --out parameter.
-
-         INPS+=("$2")
-         INPCNT=$((INPCNT + 1))
+         NINP=$((NINP + 1))
+         if [[ "$NINP" > 1 ]] # We are done with the previous input file, flush its plotting line.
+         then
+             LINE_SEP=$',\\\n'
+             flush_line "$LINE_SEP"
+             reset
+         fi
+         INP="$2"
+         shift 2
+         ;;
+      --out) # Output file.
+         OUT="$2"
          shift 2
          ;;
       -h | --help)
@@ -58,6 +122,20 @@ do
 	  ;;
     esac
 done
+
+LINE_SEP="" # Empty
+flush_line "$LINE_SEP"
+
+YR=""
+test "$YRANGE" != "none" && YR=" $YRANGE"
+XR=""
+test "$XRANGE" != "none" && XR=" $XRANGE"
+test "$YRANGE" != "none" && test "$XRANGE" = "none" && XR=" []"
+PLOTACC="$PLOTACC$XR$YR"
+
+ACC="$PLOTACC "$'\\\n'"$LINEACC"
+
+echo "$ACC"
 
 exit 0
 
