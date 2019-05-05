55bf31aea423e0aa92baee07f999cd44bdd69ca9
Cosmin Deaconu <cozzyd@kicp.uchicago.edu>
2019-05-04 22:56:06 -0500

    optimize vector class a bit, add Orthogonal()

--- vector.hh
+++ vector.hh
@@ -26,32 +26,69 @@ using std::ostream;
 //! This class represents a three-vector.  Operators are overloaded to provide for the familiar operations of vector addition, subtraction, scalar multiplication and division, and the dot product.
 class Vector {
 public:
-  friend Vector operator +(const Vector& vector1, const Vector& vector2);
+  friend Vector operator +(const Vector& vector1, const Vector& vector2)
+  {
+	return Vector(vector1.x + vector2.x , vector1.y + vector2.y , vector1.z + vector2.z);
+  }
   //Add two vectors component by component
 
-  friend void operator +=(Vector& vector1, const Vector& vector2);
+  friend void operator +=(Vector& vector1, const Vector& vector2)
+  {
+    vector1 = vector1 + vector2;
+  }
 
-  friend Vector operator -(const Vector& vector1, const Vector& vector2);
+  friend Vector operator -(const Vector& vector1, const Vector& vector2)
+  {
+    return Vector(vector1.x - vector2.x , vector1.y - vector2.y , vector1.z - vector2.z);
+  }
   //Subtract two vectors component by component
 
-  friend void operator -=(Vector& vector1, const Vector& vector2);
+  friend void operator -=(Vector& vector1, const Vector& vector2)
+  {
+    vector1 = vector1 - vector2;
+  }
 
-  friend Vector operator -(const Vector& vec);
+  friend Vector operator -(const Vector& vec)
+  {
+    return Vector(-vec.x,-vec.y,-vec.z);
+  }
   //Gives the negative of a vector
 
-  friend double operator *(const Vector& vector1, const Vector& vector2);
+  friend double operator *(const Vector& vector1, const Vector& vector2)
+  {
+    return ((vector1.x * vector2.x) + (vector1.y * vector2.y) + (vector1.z * vector2.z));
+  }
+
   //Take the dot product of two vectors
 
-  friend Vector operator /(const Vector &v, const double& a);
+  friend Vector operator /(const Vector &v, const double& a)
+  {
+    return Vector(v.x / a, v.y / a, v.z / a);
+  }
   //Divide a vector by a scalar
 
-  friend Vector operator *(const double& a, const Vector& v);
+  friend Vector operator *(const double& a, const Vector& v)
+  {
+    return Vector(a*v.x , a*v.y , a*v.z);
+  }
   //Multiply a vector by a scalar
 
   friend ostream& operator <<(ostream& outs, const Vector& vec);
   //Print the three rectangular components of the vector to the screen
 
-  double operator [](int i) const;
+  double operator [](int i) const
+  {
+    switch(i) 
+    { 
+      case 0: return x; 
+      case 1: return y; 
+      case 2: return z; 
+      case 3: return phi; 
+      case 4: return theta; 
+      default: 
+        return 0; 
+    }
+  }
   //Returns an element of the vector.  vector[0] is the x component,
   //vector[1] is the y component, and vector[2] is the z component.
 
@@ -98,6 +135,8 @@ public:
   Vector Zero();
   //zero the vector
 
+  double Mag2() const { return x*x* + y*y + z*z; }
+
   double Mag() const;
   //Returns the magnitude of this vector.
 
@@ -110,34 +149,51 @@ public:
   //The vector is rotated in such a way that a vector pointing in the z direction will be 
   //rotated onto the new axis.
 
+  Vector Orthogonal() const; 
+  // Returns a vector orthogonal to this one. 
+
+
   Vector Unit() const;
   //Returns a unit vector in the same direction as this vector.
 
   //Accessor functions
-  double GetX() const;
-  double GetY() const;
-  double GetZ() const;
+  double GetX() const { return x; }
+  double GetY() const { return y; }
+  double GetZ() const { return z; } 
+  double X() const { return x; }
+  double Y() const { return y; }
+  double Z() const { return z; } 
+
+
+
   double Theta() const;
   double Phi() const;
   void Print() const;
 
   //Mutator functions
-  void SetX(double inp);
-  void SetY(double inp);
-  void SetZ(double inp);
-  void SetXYZ(double inpx,double inpy,double inpz);
-  void Reset(double x_inp, double y_inp, double z_inp);
+  void SetX(double inp) { x = inp; angles_need_updating = true ;}
+  void SetY(double inp) { y = inp; angles_need_updating = true ;}
+  void SetZ(double inp) { z = inp; angles_need_updating = true ;}
+  void SetXYZ(double inpx,double inpy,double inpz)
+  {
+    x = inpx; 
+    y = inpy; 
+    z = inpz; 
+    angles_need_updating = true; 
+  }
+  void Reset(double x_inp, double y_inp, double z_inp) { SetXYZ(x_inp, y_inp, z_inp); } 
 
 protected:
   //Class variables
   double x; //x component of vector
   double y; //y component of vector
   double z; //z component of vector
-  double theta;  //theta component of vector in radians
-  double phi; //phi component of vector in radians
+  mutable double theta;  //theta component of vector in radians
+  mutable double phi; //phi component of vector in radians
+  mutable bool angles_need_updating; 
 
   //Class private functions
-  void UpdateThetaPhi(); 
+  void UpdateThetaPhi() const; 
   //This method finds theta and phi from the x,y,z Cartesian coordinates.  
   //It should be called at any time that the x,y,z components are modified,
   //so that the theta and phi components are current at all times.
