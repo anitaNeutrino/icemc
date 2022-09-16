fb0ad49068b49170837f2830f4b2e82cb2c695df
jwruss <jrussell09@gmail.com>
2022-09-15 16:15:17 -1000

    First pass at making changes from icemc using a static to shared library. Doesn't work yet.

--- LinkDef.h
+++ LinkDef.h
@@ -1,8 +1,15 @@
 #ifdef __CINT__
+
+#pragma link off all globals;
+#pragma link off all classes;
+#pragma link off all functions;
+#pragma link off all namespaces;
+
 #pragma link C++ class RX+;
 #pragma link C++ class Taumodel+;
 #pragma link C++ class Settings+;
 #pragma link C++ struct FAVAEntry+; 
 #pragma link C++ class TRandomXoshiro256Plus+; 
-#pragma link C++ struct icemodel_debug; 
+#pragma link C++ struct icemodel_debug;
+#pragma link C++ class EarthModel+;
 #endif
